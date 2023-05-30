d8b31a3975abd7ab598ca6ce8d1413962cf29ece
Robin lundell <robin.lundell@hotmail.com>
2023-05-30 08:58:28 +0200

    Finish VG

--- src/main/java/com/example/purchasems/WebSecurityConfig.java
+++ src/main/java/com/example/purchasems/WebSecurityConfig.java
@@ -5,19 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer(){
-//        return web -> web.ignoring().requestMatchers("/","/v3/api-docs", "/swagger-ui/index");
-//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -31,5 +24,4 @@ public class WebSecurityConfig {
         http.cors().disable().csrf().disable();
         return http.build();
     }
-
 }
